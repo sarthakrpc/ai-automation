4bfa434174f6b71206c07a526958dc21cbd8c164
Sarthak <sarthakkumar766@gmail.com>
2025-06-14 15:31:55 +0530

    first commit

---
+++ frontend/src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { OpenBrowser } from '../lib/wailsjs/go/main/App';
+
+	function openLink() {
+		OpenBrowser('https://example.com');
+	}
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<button class="btn" onclick={openLink}>Click</button>
